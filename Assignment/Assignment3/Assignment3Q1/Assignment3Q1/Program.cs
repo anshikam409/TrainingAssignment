dee0a304719f4ae8679b1728edf2adbb572cee6b
Anshika Mishra <anshikam@infinite.com>
2023-09-08 13:09:54 +0530

    My Assignment

---
+++ Assignment/Assignment3/Assignment3Q1/Assignment3Q1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string n = Console.ReadLine();
+            int len = WordLen(n);
+            Console.WriteLine($"The length of the word '{n}' is {len}.");
+            Console.ReadLine();
+        }
+        static int WordLen(string word)
+        {
+            return word.Length;
+        }
+    }
+}
---
+++ Assignment/Assignment3/Assignment3Q2/Assignment3Q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string n = Console.ReadLine();
+            string revWord = ReverseWord(n);
+            Console.WriteLine($"The reverse of the word '{n}' is '{revWord}'.");
+            Console.ReadLine();
+        }
+        static string ReverseWord(string word)
+        {
+            char[] chararr = word.ToCharArray();
+            Array.Reverse(chararr);
+            return new string(chararr);
+        }
+    }
+}
