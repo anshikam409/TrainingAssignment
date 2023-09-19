501857174c8ad3aedd8c98db640537462256cb02
anshikam409 <144266091+anshikam409@users.noreply.github.com>
2023-09-19 10:52:26 +0530

    Add files via upload

---
+++ CodebasedTest/CodebasedTest/CsharpCodebasedTest2/CsharpCodebasedTest2Q1/CsharpCodebasedTest2Q1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CsharpCodebasedTest2Q1
+{
+    public abstract class Student
+    {
+        public string Name;
+        public int StudentId;
+        public double Grade;
+        public Student(string name, int studentId)
+        {
+            Name = name;
+            StudentId = studentId;
+        }
+        public abstract bool IsPassed(double grade);
+    }
+    public class Undergraduate : Student
+    {
+        public Undergraduate(string name, int studentId)
+            : base(name, studentId)
+        {
+        }
+        public override bool IsPassed(double grade)
+        {
+            return grade >= 70.0;
+        }
+    }
+    public class Graduate : Student
+    {
+        public Graduate(string name, int studentId)
+            : base(name, studentId)
+        {
+        }
+        public override bool IsPassed(double grade)
+        {
+            return grade >= 80.0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter details for Undergraduate student:");
+            Console.Write("Name: ");
+            string undergradName = Console.ReadLine();
+            Console.Write("Student ID: ");
+            int undergradId = int.Parse(Console.ReadLine());
+            Console.Write("Grade: ");
+            double undergradGrade = double.Parse(Console.ReadLine());
+            Undergraduate undergrad = new Undergraduate(undergradName, undergradId);
+            bool undergradPassed = undergrad.IsPassed(undergradGrade);
+            Console.WriteLine($"Undergraduate {undergrad.Name} gradepassed: {undergradPassed}");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("Enter details for Graduate student:");
+            Console.Write("Name: ");
+            string gradName = Console.ReadLine();
+            Console.Write("Student ID: ");
+            int gradId = int.Parse(Console.ReadLine());
+            Console.Write("Grade: ");
+            double gradGrade = double.Parse(Console.ReadLine());
+            Graduate grad = new Graduate(gradName, gradId);
+            bool gradPassed = grad.IsPassed(gradGrade);
+            Console.WriteLine($"Graduate {grad.Name} gradepassed: {gradPassed}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodebasedTest/CodebasedTest/CsharpCodebasedTest2/CsharpCodebasedTest2Q2/CsharpCodebasedTest2Q2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CsharpCodebasedTest2Q2
+{
+    public class Product
+    {
+        public int ProductId;
+        public string ProductName;
+        public double Price;
+        public Product(int productId, string productName, double price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Product ID: {ProductId}, Product Name: {ProductName}, Price: {Price:C}";
+        }
+
+    }
+    public class ProductCollector
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            for (int i = 0; i <10; i++)
+            {
+                Console.WriteLine($"Enter details for Product {i + 1}:");
+                Console.Write("Product ID (Enter numbers): ");
+                if (!int.TryParse(Console.ReadLine(), out int productId))
+                {
+                    Console.WriteLine("Invalid input for Product ID. Please enter a number.");
+                    i--;
+                    continue;
+                }
+                Console.Write("Product Name (Enter text): ");
+                string productName = Console.ReadLine();
+                Console.Write("Price (Enter numbers): ");
+                if (!double.TryParse(Console.ReadLine(), out double price))
+                {
+                    Console.WriteLine("Invalid input for Price. Please enter a number.");
+                    i--;
+                    continue;
+                }
+                products.Add(new Product(productId, productName, price));
+            }
+            products = products.OrderBy(p => p.Price).ToList();
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("\nSorted Products by Price:");
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+            Console.ReadLine();
+        }
+    }
+}
