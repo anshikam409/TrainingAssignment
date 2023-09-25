c4839873a18303aa9078b9c7cba84135de2548ac
Anshika Mishra <anshikam@infinite.com>
2023-09-25 13:08:08 +0530

    CsharpTest3

---
+++ CsharpCodebasedTest/CsharpCodebasedTest3/CSharpCodebasedTest3Q1/CSharpCodebasedTest3Q1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CSharpCodebasedTest3Q1
+{
+    class Cricket
+    {
+        public void PointsCalculation(int no_of_matches)
+        {
+            int[] scores = new int[no_of_matches];
+            int sum = 0;
+            for (int i = 0; i < no_of_matches; i++)
+            {
+                Console.Write($"Enter score for match {i + 1}: ");
+                if (int.TryParse(Console.ReadLine(), out int score))
+                {
+                    scores[i] = score;
+                    sum += score;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid score.");
+                    i--;
+                }
+            }
+            double average = (double)sum / no_of_matches;
+            Console.WriteLine("******************************************");
+            Console.WriteLine($"Sum of scores: {sum}");
+            Console.WriteLine($"Average score: {average:F2}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of IPL matches: ");
+            if (int.TryParse(Console.ReadLine(), out int no_of_matches) && no_of_matches > 0)
+            {
+                Cricket cricket = new Cricket();
+                cricket.PointsCalculation(no_of_matches);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number of matches.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ CsharpCodebasedTest/CsharpCodebasedTest3/CSharpCodebasedTest3Q2/CSharpCodebasedTest3Q2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CSharpCodebasedTest3Q2
+{
+    class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+        public Box(double l, double b)
+        {
+            Length = l;
+            Breadth = b;
+        }
+        public static Box Add(Box box1, Box box2)
+        {
+            double newLength = box1.Length + box2.Length;
+            double newBreadth = box1.Breadth + box2.Breadth;
+            return new Box(newLength, newBreadth);
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Length: {Length}, Breadth: {Breadth}");
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Box 1: ");
+            if (double.TryParse(Console.ReadLine(), out double length1))
+            {
+                Console.Write("Enter the breadth of Box 1: ");
+                if (double.TryParse(Console.ReadLine(), out double breadth1))
+                {
+                    Console.WriteLine();
+                    Console.Write("Enter the length of Box 2: ");
+                    if (double.TryParse(Console.ReadLine(), out double length2))
+                    {
+                        Console.Write("Enter the breadth of Box 2: ");
+                        if (double.TryParse(Console.ReadLine(), out double breadth2))
+                        {
+                            Box box1 = new Box(length1, breadth1);
+                            Box box2 = new Box(length2, breadth2);
+                            Box box3 = Box.Add(box1, box2);
+                            Console.WriteLine("********************************************");
+                            Console.WriteLine("Box 1:");
+                            box1.Display();
+                            Console.WriteLine();
+                            Console.WriteLine("Box 2:");
+                            box2.Display();
+                            Console.WriteLine("********************************************");
+                            Console.WriteLine("Box 3 (Sum of Box 1 and Box 2):");
+                            box3.Display();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input for Box 2 breadth.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input for Box 2 length.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for Box 1 breadth.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for Box 1 length.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
