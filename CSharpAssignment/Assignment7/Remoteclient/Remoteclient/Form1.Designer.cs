5193e5c7fa621ae71762e62bc3e13a196b720c61
Anshika Mishra <anshikam@infinite.com>
2023-09-28 12:56:45 +0530

    Assignment7

---
+++ CSharpAssignment/Assignment7/Remoteclient/Remoteclient/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+using System;
+
+namespace Remoteclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnhighestnum = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txtresult = new System.Windows.Forms.TextBox();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnhighestnum
+            // 
+            this.btnhighestnum.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnhighestnum.Location = new System.Drawing.Point(359, 254);
+            this.btnhighestnum.Name = "btnhighestnum";
+            this.btnhighestnum.Size = new System.Drawing.Size(125, 32);
+            this.btnhighestnum.TabIndex = 0;
+            this.btnhighestnum.Text = "Click Here";
+            this.btnhighestnum.UseVisualStyleBackColor = false;
+            this.btnhighestnum.Click += new System.EventHandler(this.btnhighestnum_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1.Location = new System.Drawing.Point(105, 61);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(177, 22);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Enter the First Number-";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl2.Location = new System.Drawing.Point(105, 177);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(201, 22);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Enter the Second Number-";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl3.Location = new System.Drawing.Point(105, 338);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(177, 22);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "The Highest Number is-";
+            // 
+            // txtresult
+            // 
+            this.txtresult.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtresult.Location = new System.Drawing.Point(576, 338);
+            this.txtresult.Name = "txtresult";
+            this.txtresult.Size = new System.Drawing.Size(100, 26);
+            this.txtresult.TabIndex = 4;
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtnum1.Location = new System.Drawing.Point(576, 61);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(100, 26);
+            this.txtnum1.TabIndex = 5;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtnum2.Location = new System.Drawing.Point(576, 173);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(100, 26);
+            this.txtnum2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.txtresult);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnhighestnum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnhighestnum;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txtresult;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+    }
+}
+
