6908e3fac244dbae88a2ff2acca109dd826da2fb
Anshika Mishra <anshikam@infinite.com>
2023-10-31 16:47:15 +0530

    aspAssignment1

---
+++ ASP.netAssignment1/aspAssignment1/aspAssignment1/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="aspAssignment1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Welcome to my Mc Store</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="ddlItems" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlItems_SelectedIndexChanged">
+                <asp:ListItem Text=" Mc Menu Card" Value="" />
+                <asp:ListItem Text="Diet Coke" Value="coke" />
+                <asp:ListItem Text="Pizza" Value="pizza" />
+                <asp:ListItem Text="Oreo" Value="oreo" />
+                <asp:ListItem Text="McFlurry" Value="mcflurry" />
+                <asp:ListItem Text="Burger" Value="burger" />
+            </asp:DropDownList>
+            <br />
+            <asp:Image ID="imgItem" runat="server" Width="200px" Height="200px" ImageUrl="~/Images/placeholder.jpg" />
+            <br />
+            <asp:Button ID="btnShowCost" runat="server" Text="Show Cost" OnClick="btnShowCost_Click" />
+            <br />
+            <asp:Label ID="lblCost" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.netAssignment1/aspAssignment1/aspAssignment1/WebForm1.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aspAssignment1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void ddlItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedValue = ddlItems.SelectedValue;
+            if (!string.IsNullOrEmpty(selectedValue))
+            {
+                string imageUrl = GetImageUrl(selectedValue);
+                imgItem.ImageUrl = imageUrl;
+            }
+            else
+            {
+                imgItem.ImageUrl = "~/Images/placeholder.jpg";
+            }
+        }
+        protected void btnShowCost_Click(object sender, EventArgs e)
+        {
+            string selectedValue = ddlItems.SelectedValue;
+            if (!string.IsNullOrEmpty(selectedValue))
+            {
+                decimal itemCost = GetItemCost(selectedValue);
+                lblCost.Text = "Cost: $" + itemCost.ToString();
+            }
+            else
+            {
+                lblCost.Text = "Please select an item.";
+            }
+        }
+
+        private string GetImageUrl(string selectedItem)
+        {
+            return "~/Images/" + selectedItem.ToLower() + ".jpg";
+        }
+
+        private decimal GetItemCost(string selectedItem)
+        {
+            switch (selectedItem)
+            {
+                case "coke":
+                    return 100.00m; 
+                case "pizza":
+                    return 600.00m; // Pizza price: $600
+                case "oreo":
+                    return 150.00m; // Oreo price: $150
+                case "mcflurry":
+                    return 150.00m; // McFlurry price: $150
+                case "burger":
+                    return 200.00m; // Burger price: $200
+                default:
+                    return 0.00m; // Default to 0 if item not found
+            }
+        }
+    }
+}
