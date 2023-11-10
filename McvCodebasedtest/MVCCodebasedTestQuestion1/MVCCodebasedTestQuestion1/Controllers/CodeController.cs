ed829fbe7ccf0bd9a6136c014e34a0fd994874e6
Anshika Mishra <anshikam@infinite.com>
2023-11-10 18:17:28 +0530

    MVCC

---
+++ McvCodebasedtest/MVCCodebasedTestQuestion1/MVCCodebasedTestQuestion1/Controllers/CodeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCodebasedTestQuestion1.Controllers
+{
+    public class CodeController : Controller
+    {
+        
+        private NorthwindEntities dbContext = new NorthwindEntities();
+
+     
+        public ActionResult CustomersInGermany()
+        {
+            var customers = dbContext.Customers.Where(c => c.Country == "Germany").ToList();
+            return View(customers);
+        }
+
+      
+        public ActionResult CustomerDetailsByOrderId()
+        {
+            var customerDetails = dbContext.Customers
+                .Join(dbContext.Orders,
+                    customer => customer.CustomerID,
+                    order => order.CustomerID,
+                    (customer, order) => new { Customer = customer, Order = order })
+                .Where(result => result.Order.OrderID == 10248)
+                .ToList();
+
+            return View(customerDetails);
+        }
+    }
+
+}
